969539ecf97cb3845663af9203c8f1b7a96da14c
zhiqian.liu <zhiqian.liu@baifendian.com>
2021-08-08 16:47:46 +0800

    [开发] 多选组件开发

---
+++ packages/adhere-ui-polygonselection/src/defaultAnchorStyle.ts
@@ -0,0 +1,11 @@
+export default {
+  fillStyle: '#fff',
+  // 描边颜色
+  strokeStyle: '#000',
+  // 描边大小
+  lineWidth: 1,
+  lineCap: 'round',
+  lineJoin: 'round',
+  lineDash: [],
+  lineDashOffset: -1,
+};
