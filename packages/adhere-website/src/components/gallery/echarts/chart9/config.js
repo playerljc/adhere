47d014853e4a677a654234a7c43ab96262d01a1f
ying.li <ying.li@baifendian.com>
2021-08-30 15:46:49 +0800

    task 【地图】 echarts地图

---
+++ packages/adhere-website/src/components/gallery/echarts/chart9/config.js
@@ -0,0 +1,102 @@
+const data = [
+  {
+    name: '南宁市',
+    center: [108.320004, 22.82402],
+    centroid: [108.467546, 23.055985],
+    accidentNum: 70,
+    dieNum: 70,
+  },
+  {
+    name: '柳州市',
+    center: [109.411703, 24.314617],
+    centroid: [109.374902, 24.94791],
+    accidentNum: 70,
+    dieNum: 70,
+  },
+  {
+    name: '桂林市',
+    center: [110.299121, 25.274215],
+    centroid: [110.518333, 25.35549],
+    accidentNum: 70,
+    dieNum: 70,
+  },
+  {
+    name: '梧州市',
+    center: [111.297604, 23.474803],
+    centroid: [110.99512, 23.479776],
+    accidentNum: 70,
+    dieNum: 70,
+  },
+  {
+    name: '北海市',
+    center: [109.119254, 21.473343],
+    centroid: [109.343711, 21.665234],
+    accidentNum: 70,
+    dieNum: 70,
+  },
+  {
+    name: '防城港市',
+    center: [108.345478, 21.614631],
+    centroid: [108.016067, 21.869663],
+    accidentNum: 70,
+    dieNum: 70,
+  },
+  {
+    name: '钦州市',
+    center: [108.624175, 21.967127],
+    centroid: [109.020264, 22.171739],
+    accidentNum: 70,
+    dieNum: 70,
+  },
+  {
+    name: '贵港市',
+    center: [109.602146, 23.0936],
+    centroid: [110.00316, 23.307229],
+    accidentNum: 70,
+    dieNum: 70,
+  },
+  {
+    name: '玉林市',
+    center: [110.154393, 22.63136],
+    centroid: [110.187226, 22.440758],
+    accidentNum: 70,
+    dieNum: 70,
+  },
+  {
+    name: '百色市',
+    center: [106.616285, 23.897742],
+    centroid: [106.289767, 23.986179],
+    accidentNum: 70,
+    dieNum: 70,
+  },
+  {
+    name: '贺州市',
+    center: [111.552056, 24.414141],
+    centroid: [111.366008, 24.358064],
+    accidentNum: 70,
+    dieNum: 70,
+  },
+  {
+    name: '河池市',
+    center: [108.062105, 24.695899],
+    centroid: [107.845852, 24.642393],
+    accidentNum: 70,
+    dieNum: 70,
+  },
+  {
+    name: '来宾市',
+    center: [109.229772, 23.733766],
+    centroid: [109.416662, 23.851919],
+    accidentNum: 70,
+    dieNum: 70,
+  },
+  {
+    name: '崇左市',
+    center: [107.353926, 22.404108],
+    centroid: [107.277353, 22.502179],
+    accidentNum: 70,
+    dieNum: 70,
+  }
+]
+
+export default data;
