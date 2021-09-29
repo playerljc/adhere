3006791891f71bc3576ae0f59e34bd0eeb56d831
zhiqian.liu <zhiqian.liu@baifendian.com>
2021-09-29 22:23:38 +0800

    [开发] demo修改

--- packages/adhere-website/src/app.jsx
+++ packages/adhere-website/src/app.jsx
@@ -29,43 +29,43 @@ class App extends React.PureComponent {
                   className="tanuki-shape"
                   fill="#e24329"
                   d="M2 14l9.38 9v-9l-4-12.28c-.205-.632-1.176-.632-1.38 0z"
-                ></path>
+                />
                 <path
                   id="tanuki-left-ear"
                   className="tanuki-shape"
                   fill="#e24329"
                   d="M34 14l-9.38 9v-9l4-12.28c.205-.632 1.176-.632 1.38 0z"
-                ></path>
+                />
                 <path
                   id="tanuki-nose"
                   className="tanuki-shape"
                   fill="#e24329"
                   d="M18,34.38 3,14 33,14 Z"
-                ></path>
+                />
                 <path
                   id="tanuki-right-eye"
                   className="tanuki-shape"
                   fill="#fc6d26"
                   d="M18,34.38 11.38,14 2,14 6,25Z"
-                ></path>
+                />
                 <path
                   id="tanuki-left-eye"
                   className="tanuki-shape"
                   fill="#fc6d26"
                   d="M18,34.38 24.62,14 34,14 30,25Z"
-                ></path>
+                />
                 <path
                   id="tanuki-right-cheek"
                   className="tanuki-shape"
                   fill="#fca326"
                   d="M2 14L.1 20.16c-.18.565 0 1.2.5 1.56l17.42 12.66z"
-                ></path>
+                />
                 <path
                   id="tanuki-left-cheek"
                   className="tanuki-shape"
                   fill="#fca326"
                   d="M34 14l1.9 6.16c.18.565 0 1.2-.5 1.56L18 34.38z"
-                ></path>
+                />
               </svg>
             </a>
           </div>
