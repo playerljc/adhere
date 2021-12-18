ab1d566e9771b7e2ef11ad03467a16a245d80269
zhiqian.liu <1qaz@WSX>
2021-12-18 09:00:30 +0800

    去掉无用代码

--- packages/adhere-website/src/components/gallery/graph/manifest.jsx
+++ packages/adhere-website/src/components/gallery/graph/manifest.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Graph1 from './graph1';
 import Windmill from './Windmill';
 
 export default [
@@ -13,9 +12,4 @@ export default [
       </div>
     ),
   },
-  // {
-  //   title: 'Graph1',
-  //   keyword: '多边形选取',
-  //   component: <Graph1 />,
-  // },
 ];
