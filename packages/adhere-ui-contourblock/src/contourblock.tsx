dc3172f25a9d80dc229854a75b071446fd53d066
zhiqian.liu <1qaz@WSX>
2022-09-06 14:49:09 +0800

    refactor: 重构 1.adhere-ui-contourblock

--- packages/adhere-ui-contourblock/src/contourblock.tsx
+++ packages/adhere-ui-contourblock/src/contourblock.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { FC } from 'react';
 
-import { IContourBlockProps } from './types';
+import { ContourBlockProps } from './types';
 
 const selectorPrefix = 'adhere-ui-contourblock';
 
@@ -9,24 +8,16 @@ const selectorPrefix = 'adhere-ui-contourblock';
  * ContourBlock
  * @constructor
  */
-function ContourBlock(props: IContourBlockProps) {
+const ContourBlock: FC<ContourBlockProps> = (props) => {
+  const { className, style, children } = props;
+
   return (
-    <div className={props.className || ''} style={props.style || {}}>
+    <div className={className || ''} style={style || {}}>
       <div className={`${selectorPrefix}`}>
-        <div className={`${selectorPrefix}-inner`}>{props.children}</div>
+        <div className={`${selectorPrefix}-inner`}>{children}</div>
       </div>
     </div>
   );
-}
-
-ContourBlock.defaultProps = {
-  className: '',
-  style: {},
-};
-
-ContourBlock.propTypes = {
-  className: PropTypes.string,
-  style: PropTypes.object,
 };
 
 export default ContourBlock;
