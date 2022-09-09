5b56e55a8828c2df7d56ac290ba842a069cea362
zhiqian.liu <1qaz@WSX>
2022-09-09 11:06:50 +0800

    refactor: 重构 1.adhere-ui-spin

--- packages/adhere-ui-spin/src/spin.tsx
+++ packages/adhere-ui-spin/src/spin.tsx
@@ -25,43 +25,4 @@ const Spin: FC<SpinProps> = (props) => {
   ) : null;
 };
 
-// /**
-//  * Spin
-//  * @class Spin
-//  * @classdesc Spin
-//  */
-// class Spin extends React.Component<ISpinProps, any> {
-//   static defaultProps: any;
-//   static propTypes: any;
-//
-//   render() {
-//     // @ts-ignore
-//     const { spinning, text, zIndex } = this.props;
-//
-//     return spinning ? (
-//       <div className={selectorPrefix} style={{ zIndex: zIndex }}>
-//         <span className={`${selectorPrefix}-dot`}>
-//           <i></i>
-//           <i></i>
-//           <i></i>
-//           <i></i>
-//         </span>
-//         <div className={`${selectorPrefix}-text`}>{text}</div>
-//       </div>
-//     ) : null;
-//   }
-// }
-//
-// Spin.defaultProps = {
-//   spinning: false,
-//   text: '',
-//   zIndex: Resource.Dict.value.ResourceNormalMaxZIndex.value,
-// };
-//
-// Spin.propTypes = {
-//   spinning: PropTypes.bool,
-//   text: PropTypes.string,
-//   zIndex: PropTypes.number,
-// };
-
 export default Spin;
