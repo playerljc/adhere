573acae1d12995451a982e35d1b9b0643dd775a2
zhiqian.liu <1qaz@WSX>
2022-09-09 11:15:33 +0800

    refactor: 重构 1.adhere-ui-split

--- packages/adhere-ui-split/es/split.d.ts
+++ packages/adhere-ui-split/es/split.d.ts
@@ -1,35 +1,3 @@
-import React from 'react';
-import { ISplitProps } from './types';
-/**
- * SplitGroup
- * @class SplitGroup
- * @classdesc SplitGroup
- */
-declare class SplitGroup extends React.Component<ISplitProps, any> {
-    static defaultProps: any;
-    static propTypes: any;
-    render(): JSX.Element;
-}
-/**
- * Split
- * @class Split
- * @classdesc Split
- */
-declare class Split extends React.Component<ISplitProps, any> {
-    static defaultProps: any;
-    static propTypes: any;
-    static Group: typeof SplitGroup;
-    getStyle(): {
-        display: string;
-        width: number;
-        height: string;
-        margin: string;
-    } | {
-        width: string;
-        height: number;
-        margin: string;
-        display?: undefined;
-    };
-    render(): React.ReactElement;
-}
+import { SplitFunction, SplitProps } from './types';
+declare const Split: SplitFunction<SplitProps>;
 export default Split;
