b3648a0289935a8141154484112d60559cdd271c
zhiqian.liu <1qaz@WSX>
2022-09-09 10:25:45 +0800

    refactor: 重构 1.adhere-ui-space

--- packages/adhere-ui-space/lib/space.d.ts
+++ packages/adhere-ui-space/lib/space.d.ts
@@ -1,35 +1,3 @@
-import React from 'react';
-import { ISpaceProps } from './types';
-/**
- * SpaceGroup
- * @class SpaceGroup
- * @classdesc SpaceGroup
- */
-declare class SpaceGroup extends React.Component<ISpaceProps, any> {
-    static defaultProps: any;
-    static propTypes: any;
-    render(): JSX.Element;
-}
-/**
- * Space
- * @class Space
- * @classdesc Space
- */
-declare class Space extends React.Component<ISpaceProps, any> {
-    static defaultProps: any;
-    static propTypes: any;
-    static Group: typeof SpaceGroup;
-    getStyle(): {
-        display: string;
-        height: string;
-        margin: string;
-        width?: undefined;
-    } | {
-        width: string;
-        margin: string;
-        display?: undefined;
-        height?: undefined;
-    };
-    render(): React.ReactElement;
-}
+import { SpaceFunction, SpaceProps } from './types';
+declare const Space: SpaceFunction<SpaceProps>;
 export default Space;
