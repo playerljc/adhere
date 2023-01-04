4b6abad882f1626be77a0cf4d6dd21318066d284
zhiqian.liu <1qaz@WSX>
2023-01-04 16:35:42 +0800

    fix: demo修改

--- packages/adhere-website/src/components/util/resource/index.jsx
+++ packages/adhere-website/src/components/util/resource/index.jsx
@@ -3413,42 +3413,42 @@ export default () => {
     Dict.handlers.ResourceMomentFormat6 =
       () =>
       (split = '-') =>
-        \`YYYY${split}M\`;
+        \`YYYY\${split}M\`;
 
     Dict.handlers.ResourceMomentFormat7 =
       () =>
       (split = '-') =>
-        \`YYYY${split}MM\`;
+        \`YYYY\${split}MM\`;
 
     Dict.handlers.ResourceMomentFormat8 =
       () =>
       (split = '-') =>
-        \`YYYY${split}M${split}D\`;
+        \`YYYY\${split}M\${split}D\`;
 
     Dict.handlers.ResourceMomentFormat10 =
       () =>
       (split = '-') =>
-        \`YYYY${split}MM${split}DD\`;
+        \`YYYY\${split}MM\${split}DD\`;
 
     Dict.handlers.ResourceMomentFormat13 =
       () =>
       (split1 = '-', split2 = ':') =>
-        \`YYYY${split1}M${split1}D H${split2}m${split2}s\`;
+        \`YYYY\${split1}M\${split1}D H\${split2}m\${split2}s\`;
 
     Dict.handlers.ResourceMomentFormat15 =
       () =>
       (split1 = '-', split2 = ':') =>
-        \`YYYY${split1}MM${split1}DD H${split2}m${split2}s\`;
+        \`YYYY\${split1}MM\${split1}DD H\${split2}m\${split2}s\`;
 
     Dict.handlers.ResourceMomentFormat16 =
       () =>
       (split1 = '-', split2 = ':') =>
-        \`YYYY${split1}M${split1}D HH${split2}mm${split2}ss\`;
+        \`YYYY\${split1}M\${split1}D HH\${split2}mm\${split2}ss\`;
 
     Dict.handlers.ResourceMomentFormat18 =
       () =>
       (split1 = '-', split2 = ':') =>
-        \`YYYY${split1}MM${split1}DD HH${split2}mm${split2}ss\`;
+        \`YYYY\${split1}MM\${split1}DD HH\${split2}mm\${split2}ss\`;
           `,
           },
         ],
