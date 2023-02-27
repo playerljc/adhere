6f928d0bade7a04bda87d661ea1e0c9102e25893
zhiqian.liu <1qaz@WSX>
2023-02-27 21:30:14 +0800

    feat: 加入 adhere-util-resource 去掉request.context的Ap 加入加入SubmitButton 加入AutoCompleteFormItem

---
+++ packages/adhere-ui-antdformitem/src/FormItemGeneratorToDict/AutoCompleteFormItem/index.tsx
@@ -0,0 +1,58 @@
+import React, { FC, useMemo } from 'react';
+
+import { AutoComplete } from '../../AntFormItemNormalize';
+import { AutoCompleteFormItemProps } from '../../types';
+
+/**
+ * AutoCompleteFormItem
+ * @param props
+ * @return {JSX.Element}
+ * @constructor
+ */
+const AutoCompleteFormItem: FC<AutoCompleteFormItemProps> = (props) => {
+  // 选择Select的时候调用
+  const onSelect = (selectValue) => {
+    if (props?.onChange) {
+      props.onChange({
+        inputValue: '',
+        selectValue,
+      });
+    }
+  };
+
+  // 每次输入的时候调用
+  const onSearch = (searchText) => {
+    if (props?.onChange) {
+      props.onChange({
+        inputValue: searchText.trim(),
+        selectValue: undefined,
+      });
+    }
+  };
+
+  const value = useMemo(() => {
+    if (props.value.inputValue) return props.value.inputValue;
+
+    if (props.value.selectValue)
+      return props?.dataSource?.find((t) => t.value === props.value.selectValue)?.label;
+  }, [props.value, props.value.inputValue, props.value.selectValue]);
+
+  const options = useMemo(() => {
+    if (!props.value.inputValue) return props.dataSource || [];
+
+    return props?.dataSource?.filter?.((t) => t.label.includes(props.value.inputValue)) || [];
+  }, [props.value, props.value.inputValue, props.value.selectValue]);
+
+  return (
+    <AutoComplete
+      options={options}
+      filterOption={false}
+      onSelect={onSelect}
+      onSearch={onSearch}
+      {...props}
+      value={value}
+    />
+  );
+};
+
+export default AutoCompleteFormItem;
