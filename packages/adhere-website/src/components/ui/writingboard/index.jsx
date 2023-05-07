76d6a37acff1bb165e7ee736605f9295de9c4b8e
playerljc <playerljclzq@gmail.com>
2023-05-07 17:24:22 +0800

    test(adhere-ui-writingboard): 加入测试代码

--- packages/adhere-website/src/components/ui/writingboard/index.jsx
+++ packages/adhere-website/src/components/ui/writingboard/index.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { WritingBoard } from '@baifendian/adhere';
 
-import PlayGroundPage, { CodeBoxSection, PropsSection, Section } from '@/lib/PlaygroundPage';
+import PlayGroundPage, {
+  CodeBoxSection,
+  FunctionPropsSection,
+  PropsSection,
+  Section,
+} from '@/lib/PlaygroundPage';
 
 import Simple from './simple';
 
 export default () => {
   const ref1 = React.createRef();
+
+  const [value, setValue] = useState('');
 
   return (
     <PlayGroundPage>
@@ -223,11 +230,49 @@ export default () => {
             ],
             renderChildren: () => <Simple />,
           },
+          {
+            id: `p3`,
+            name: `签名`,
+            mode: 'code',
+            scope: { React },
+            cardProps: {
+              description: {
+                title: '签名',
+                info: '签名',
+              },
+            },
+            codeText: `
+  import React, { useState } from 'react';
+  import { WritingBoard } from '@baifendian/adhere';
+
+  export default () => {
+    const [value, setValue] = useState('');
+
+    return (
+      <div>
+        <p>签名</p>
+        <div style={{ width: 200, height: 300, border: '1px solid #ccc' }}>
+          <WritingBoard.Signature value={value} onChange={(v) => setValue(v)} />
+        </div>
+      </div>
+    )
+  };
+            `,
+            type: 'PlayGround',
+            renderChildren: () => (
+              <div>
+                <p>签名</p>
+                <div style={{ width: 200, height: 300, border: '1px solid #ccc' }}>
+                  <WritingBoard.Signature value={value} onChange={(v) => setValue(v)} />
+                </div>
+              </div>
+            ),
+          },
         ]}
       />
 
       <PropsSection
-        title="Props"
+        title="WritingBoard"
         config={[
           {
             border: true,
@@ -283,6 +328,279 @@ export default () => {
                 desc: '防抖的事件',
                 type: 'number',
                 defaultVal: '300',
+              },
+            ],
+          },
+        ]}
+      />
+
+      <PropsSection
+        title="Signature"
+        config={[
+          {
+            border: true,
+            title: '属性',
+            data: [
+              {
+                params: 'className',
+                desc: '附加的样式表',
+                type: 'string',
+                defaultVal: '',
+              },
+              {
+                params: 'style',
+                desc: '附加的样式',
+                type: 'React.CSSProperties',
+                defaultVal: '',
+              },
+              {
+                params: 'modalProps',
+                desc: '弹出窗口设置',
+                type: `ModalProps`,
+                defaultVal: '',
+              },
+              {
+                params: 'coreProps',
+                desc: 'SignatureCore设置',
+                type: 'SignatureCoreProps',
+                defaultVal: '',
+              },
+              {
+                params: 'value',
+                desc: 'base64',
+                type: 'string',
+                defaultVal: '',
+              },
+              {
+                params: 'onChange',
+                desc: '获取base64',
+                type: '(base64?: string) => void',
+                defaultVal: '',
+              },
+            ],
+          },
+        ]}
+      />
+
+      <PropsSection
+        title="SignatureCore"
+        config={[
+          {
+            border: true,
+            title: '属性',
+            data: [
+              {
+                params: 'defaultWidth',
+                desc: '线条默认宽度',
+                type: 'number',
+                defaultVal: '2',
+              },
+              {
+                params: 'defaultColor',
+                desc: '默认颜色',
+                type: 'string',
+                defaultVal: '#000',
+              },
+              {
+                params: 'wrapProps',
+                desc: '最外层配置',
+                type: `SignatureCoreWrapProps`,
+                defaultVal: '',
+              },
+              {
+                params: 'toolProps',
+                desc: '工具栏设置',
+                type: 'SignatureCoreToolProps',
+                defaultVal: '',
+              },
+              {
+                params: 'areaProps',
+                desc: '编辑区设置',
+                type: 'SignatureCoreAreaProps',
+                defaultVal: '',
+              },
+            ],
+          },
+        ]}
+      />
+
+      <FunctionPropsSection
+        title="WritingBoard"
+        config={[
+          {
+            border: true,
+            title: 'WritingBoard',
+            data: [
+              {
+                name: 'setMode',
+                desc: '设置模式',
+                modifier: 'public',
+                params: [
+                  {
+                    name: 'mode',
+                    desc: '模式',
+                    type: `
+                      {
+                        // 直线
+                        LINE = 'line',
+                        // 矩形
+                        RECTANGLE = 'rectangle',
+                        // 圆形
+                        CIRCLE = 'circle',
+                        // 三角形
+                        TRIANGLE = 'triangle',
+                        // 自由绘制
+                        FREE = 'free',
+                        // 橡皮
+                        RUBBER = 'rubber',
+                      }
+                    `,
+                    defaultVal: '',
+                    required: '',
+                  },
+                ],
+                returnType: '',
+                returnDesc: '',
+              },
+              {
+                name: 'setStrokeStyle',
+                desc: '设置描边样式',
+                modifier: 'public',
+                params: [
+                  {
+                    name: 'style',
+                    desc: '样式',
+                    type: `style`,
+                    defaultVal: '',
+                    required: '',
+                  },
+                ],
+                returnType: '',
+                returnDesc: '',
+              },
+              {
+                name: 'setLineWidth',
+                desc: '设置线条样式',
+                modifier: 'public',
+                params: [
+                  {
+                    name: 'width',
+                    desc: '样式',
+                    type: `number`,
+                    defaultVal: '',
+                    required: '',
+                  },
+                ],
+                returnType: '',
+                returnDesc: '',
+              },
+              {
+                name: 'clear',
+                desc: '清空画布',
+                modifier: 'public',
+                params: [],
+                returnType: '',
+                returnDesc: '',
+              },
+              {
+                name: 'toDataURL',
+                desc: '获取画布数据',
+                modifier: 'public',
+                params: [
+                  {
+                    name: 'backgroundColor',
+                    desc: '背景颜色',
+                    type: `string`,
+                    defaultVal: '#fff',
+                    required: '',
+                  },
+                  {
+                    name: 'type',
+                    desc: 'mimeType',
+                    type: `string`,
+                    defaultVal: 'image/png',
+                    required: '',
+                  },
+                  {
+                    name: 'quality',
+                    desc: '图片质量',
+                    type: `number`,
+                    defaultVal: '1.0',
+                    required: '',
+                  },
+                ],
+                returnType: '',
+                returnDesc: '',
+              },
+            ],
+          },
+        ]}
+      />
+
+      <FunctionPropsSection
+        title="Signature"
+        config={[
+          {
+            border: true,
+            title: 'Signature',
+            data: [
+              {
+                name: 'save',
+                desc: '保存',
+                modifier: 'public',
+                params: [
+                  {
+                    name: 'backgroundColor',
+                    desc: '背景颜色',
+                    type: `string`,
+                    defaultVal: '#fff',
+                    required: '',
+                  },
+                  {
+                    name: 'type',
+                    desc: 'mimeType',
+                    type: `string`,
+                    defaultVal: 'image/png',
+                    required: '',
+                  },
+                  {
+                    name: 'quality',
+                    desc: '图片质量',
+                    type: `number`,
+                    defaultVal: '1.0',
+                    required: '',
+                  },
+                ],
+                returnType: '',
+                returnDesc: '',
+              },
+            ],
+          },
+        ]}
+      />
+
+      <FunctionPropsSection
+        title="SignatureCore"
+        config={[
+          {
+            border: true,
+            title: 'SignatureCore',
+            data: [
+              {
+                name: 'isEmpty',
+                desc: '判断是否为空',
+                modifier: 'public',
+                params: [
+                  {
+                    name: 'value',
+                    desc: '',
+                    type: 'string',
+                    defaultVal: '',
+                    required: '',
+                  },
+                ],
+                returnType: 'boolean',
+                returnDesc: '',
               },
             ],
           },
