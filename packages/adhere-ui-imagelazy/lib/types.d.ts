8692c1c3c1a46fb38f25b05cff9e98aa87ebcdbd
playerljc <playerljclzq@gmail.com>
2023-05-24 23:30:15 +0800

    feat(adhere-ui-imagelazy): 丰富props属性

--- packages/adhere-ui-imagelazy/lib/types.d.ts
+++ packages/adhere-ui-imagelazy/lib/types.d.ts
@@ -1,11 +1,14 @@
+import React from 'react';
 /**
  * ImageLazyProps
  * @interface ImageLazyProps
  */
 export interface ImageLazyProps {
     className?: string;
+    style?: React.CSSProperties;
     imgArgs?: {
         originSrc: string;
         targetSrc: string;
+        alt?: string;
     };
 }
