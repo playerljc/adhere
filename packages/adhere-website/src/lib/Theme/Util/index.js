cf1a614e59534070dbd2525659f814ee4123ae88
playerljc <playerljclzq@gmail.com>
2023-06-04 10:53:52 +0800

    feat(demo): 主题切换修改

--- packages/adhere-website/src/lib/Theme/Util/index.js
+++ packages/adhere-website/src/lib/Theme/Util/index.js
@@ -8,8 +8,12 @@ import DefaultTheme from './default/vars';
 
 const { darkAlgorithm, defaultAlgorithm, defaultSeed } = theme;
 
+/**
+ * themes
+ */
 const themes = new Map([
   [
+    // 缺省主题
     'default',
     {
       algorithm: theme.defaultAlgorithm,
@@ -18,6 +22,7 @@ const themes = new Map([
     },
   ],
   [
+    // 暗黑主题
     'dark',
     {
       algorithm: theme.darkAlgorithm,
@@ -36,41 +41,56 @@ const exportObj = {};
 // 当前主题
 let currentTheme = 'default';
 
-const keys = Object.keys(DefaultTheme);
+/**
+ * setCssVariable
+ * @description 设置css变量
+ * @param {string} varName
+ */
+const setCssVariable = (varName) => {
+  const varCamelCaseName = Util.toCamelCase(varName);
+  const varUpperCamelCaseName = Util.toCamelCase(varName, true);
+
+  if (varCamelCaseName in cssVars) return;
+
+  // 定义驼峰变量
+  Object.defineProperty(cssVars, varCamelCaseName, {
+    set(value) {
+      // --------------定义css变量 -------------
+      document.documentElement.style.setProperty(`--${varName}`, value);
+
+      // 是颜色需要在定义一个-rgb的变量
+      const color = tinyColor(value);
+      if (color.isValid()) {
+        const rgb = color.toRgb();
+
+        document.documentElement.style.setProperty(
+          `--${varName}-rgb`,
+          `${[rgb.r, rgb.g, rgb.b].join(',')}`,
+        );
+      }
+    },
+  });
+
+  // 定义导出变量
+  exportObj[`set${varUpperCamelCaseName}`] = (value) => {
+    cssVars[varCamelCaseName] = value;
+  };
+
+  // 定义导出变量
+  exportObj[`get${varUpperCamelCaseName}`] = () =>
+    document.documentElement.style.getPropertyValue(`--${varName}`);
+};
 
 (function () {
+  const hostKeys = Object.keys(DefaultTheme);
+  const antdKeys = Object.keys(themes.get('default').mapToken);
+
   /* 全局的css Vars*/
-  keys.forEach((varName) => {
-    const varCamelCaseName = Util.toCamelCase(varName);
-    const varUpperCamelCaseName = Util.toCamelCase(varName, true);
-
-    // 定义驼峰变量
-    Object.defineProperty(cssVars, varCamelCaseName, {
-      set(value) {
-        // --------------定义css变量 -------------
-        document.documentElement.style.setProperty(`--${varName}`, value);
-
-        // 是颜色需要在定义一个-rgb的变量
-        const color = tinyColor(value);
-        if (color.isValid()) {
-          const rgb = color.toRgb();
-
-          document.documentElement.style.setProperty(
-            `--${varName}-rgb`,
-            `${[rgb.r, rgb.g, rgb.b].join(',')}`,
-          );
-        }
-      },
-    });
-
-    // 定义导出变量
-    exportObj[`set${varUpperCamelCaseName}`] = (value) => {
-      cssVars[varCamelCaseName] = value;
-    };
-
-    // 定义导出变量
-    exportObj[`get${varUpperCamelCaseName}`] = () =>
-      document.documentElement.style.getPropertyValue(`--${varName}`);
+  [
+    ...hostKeys,
+    ...antdKeys.map((_key) => Util.pascalCaseToKebabCase(`antd${Util.capitalized(_key)}`)),
+  ].forEach((varName) => {
+    setCssVariable(varName);
   });
 })();
 
@@ -81,12 +101,22 @@ const keys = Object.keys(DefaultTheme);
  */
 const init = (theme) => {
   // 主题的变量
-  const vars = (themes.get(theme) ?? themes.get('default')).token;
+  const token = (themes.get(theme) ?? themes.get('default')).token;
+  const mapToken = (themes.get(theme) ?? themes.get('default')).mapToken;
 
-  Object.keys(vars).forEach((_key) => {
+  console.log(exportObj);
+
+  Object.keys(token).forEach((_key) => {
     const varName = Util.toCamelCase(_key, true);
 
-    exportObj[`set${varName}`](vars[_key]);
+    exportObj[`set${varName}`](token[_key]);
+  });
+
+  Object.keys(mapToken).forEach((_key) => {
+    // colorPrimary
+    const varName = Util.capitalized(Util.toCamelCase(_key, true));
+
+    exportObj[`setAntd${varName}`](mapToken[_key]);
   });
 };
 
