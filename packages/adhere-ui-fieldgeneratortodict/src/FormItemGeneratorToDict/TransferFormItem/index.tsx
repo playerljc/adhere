66856c27795146c893f4d1ff836b1aaefb31a360
playerljc <playerljclzq@gmail.com>
2023-07-08 18:11:19 +0800

    fix(adhere-ui-fieldgeneratortodict): TransferFormItem加入Suspense.Sync

--- packages/adhere-ui-fieldgeneratortodict/src/FormItemGeneratorToDict/TransferFormItem/index.tsx
+++ packages/adhere-ui-fieldgeneratortodict/src/FormItemGeneratorToDict/TransferFormItem/index.tsx
@@ -1,30 +1,51 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import { Transfer } from '@baifendian/adhere-ui-anthoc';
+import Suspense from '@baifendian/adhere-ui-suspense';
 
-import { TransferFormItemProps } from '../../types';
+import { LabelValue, TransferFormItemProps } from '../../types';
 
 /**
  * TransferFormItem
+ * @param firstLoading
+ * @param renderEmpty
  * @param dataSource
  * @param props
  * @constructor
  */
-const TransferFormItem: FC<TransferFormItemProps> = ({ dataSource, ...props }) => {
+const TransferFormItem: FC<TransferFormItemProps> = ({
+  firstLoading,
+  renderEmpty,
+  dataSource,
+  ...props
+}) => {
+  const [data, setData] = useState<LabelValue[]>([]);
+
+  useEffect(() => {
+    setData(dataSource || []);
+  }, [dataSource]);
+
   return (
-    <Transfer
-      // @ts-ignore
-      dataSource={dataSource.map((t) => ({
-        ...t,
-        key: t.value,
-        title: t.label,
-        description: t.label,
-      }))}
-      // @ts-ignore
-      render={(item) => item.title}
-      targetKeys={props.value}
-      {...props}
-    />
+    <Suspense.Sync
+      data={data}
+      isEmpty={() => data.length === 0}
+      firstLoading={firstLoading}
+      renderEmpty={renderEmpty}
+    >
+      <Transfer
+        // @ts-ignore
+        dataSource={data.map((t) => ({
+          ...t,
+          key: t.value,
+          title: t.label,
+          description: t.label,
+        }))}
+        // @ts-ignore
+        render={(item) => item.title}
+        targetKeys={props.value}
+        {...props}
+      />
+    </Suspense.Sync>
   );
 };
 
