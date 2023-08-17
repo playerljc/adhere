2a9d78e6bb1a9e80b0e000eccc382e2a9f879e73
playerljc <playerljclzq@gmail.com>
2023-08-17 14:07:46 +0800

    demo(adhere-util-dict): 加入useDict的demo

--- packages/adhere-website/src/components/util/dict/index.jsx
+++ packages/adhere-website/src/components/util/dict/index.jsx
@@ -1,8 +1,9 @@
-import { Select } from 'antd';
+import { Button, Card, Select, Table } from 'antd';
+import Mock from 'mockjs';
 import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { v1 } from 'uuid';
 
-import { Dict, Space } from '@baifendian/adhere';
+import { DateDisplay, Dict, Space } from '@baifendian/adhere';
 import { Link } from '@ctsj/router';
 
 import PlayGroundPage, { CodeBoxSection, Section } from '@/lib/PlaygroundPage';
@@ -10,6 +11,8 @@ import PlayGroundPage, { CodeBoxSection, Section } from '@/lib/PlaygroundPage';
 import pca from './pca.json';
 
 const { Option } = Select;
+
+const { useDict } = Dict;
 
 export default () => {
   function boxPanelConfig() {
@@ -741,6 +744,236 @@ export default () => {
 
           // 初始化
           Dict.init([Test7Dict], {});
+
+          // 使用字典
+          return <Test />;
+        },
+      },
+      {
+        id: 'p8',
+        name: `使用useDict`,
+        mode: 'code',
+        scope: { React },
+        cardProps: {
+          description: {
+            title: '使用useDict',
+            info: '使用useDict',
+          },
+        },
+        codeText: `
+  import React, { useState } from 'react';
+  import { Table, Button, Card } from 'antd';
+  import { Dict, useDict, DateDisplay } from '@baifendian/adhere';
+  import Mock from 'mockjs';
+
+  function Test() {
+    const [params, setParams] = useState(Date.now());
+    const { data, isPending } = useDict('Test8Dict', { functionArgs: params });
+
+    return (
+      <Card
+        title="人员"
+        extra={
+          <Button
+            type="primary"
+            onClick={() => {
+              setParams(Date.now());
+            }}
+          >
+            刷新
+          </Button>
+        }
+      >
+        <Table
+          columns={[
+            {
+              title: '姓名',
+              dataIndex: 'name',
+              key: 'name',
+            },
+            {
+              title: '性别',
+              dataIndex: 'sex',
+              key: 'sex',
+              render: (v) => (v === 1 ? '男' : '女'),
+            },
+            {
+              title: '生日',
+              dataIndex: 'birthDay',
+              key: 'birthDay',
+              render: (val) => <DateDisplay.DateDisplay10 value={val} />,
+            },
+            {
+              title: '所在部门',
+              dataIndex: 'deptName',
+              key: 'deptName',
+            },
+            {
+              title: '身高',
+              dataIndex: 'height',
+              key: 'height',
+            },
+            {
+              title: '体重',
+              dataIndex: 'width',
+              key: 'width',
+            },
+            {
+              title: '籍贯',
+              dataIndex: 'hometown',
+              key: 'hometown',
+            },
+            {
+              title: '地址',
+              dataIndex: 'address',
+              key: 'address',
+            },
+          ]}
+          loading={isPending}
+          dataSource={data}
+        />
+      </Card>
+    );
+  }
+
+  const Test8Dict = {
+    initStatic() {},
+    initRemote() {
+      Dict.handlers.Test8Dict = () => () =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(
+              Array.from({ length: 10 }).map((v, index) => ({
+                id: Mock.mock('@guid'),
+                name: Mock.mock('@name'),
+                sex: \`${(index + 1) % 2}\`,
+                birthDay: Mock.mock('@now'),
+                deptName: Mock.mock('@name'),
+                height: Mock.mock({
+                  'number|+1': 202,
+                }),
+                width: Mock.mock({
+                  'number|+1': 202,
+                }),
+                hometown: Mock.mock('@city'),
+                address: Mock.mock('@region'),
+              })),
+            );
+          }, 3000);
+        });
+    },
+  };
+
+  // 初始化
+  Dict.init([Test8Dict], {});
+
+  // 使用字典
+  return () => <Test />;
+
+
+        `,
+        type: 'PlayGround',
+        renderChildren: () => {
+          function Test() {
+            const [params, setParams] = useState(Date.now());
+            const { data, isPending } = useDict('Test8Dict', { functionArgs: params });
+
+            return (
+              <Card
+                title="人员"
+                extra={
+                  <Button
+                    type="primary"
+                    onClick={() => {
+                      setParams(Date.now());
+                    }}
+                  >
+                    刷新
+                  </Button>
+                }
+              >
+                <Table
+                  columns={[
+                    {
+                      title: '姓名',
+                      dataIndex: 'name',
+                      key: 'name',
+                    },
+                    {
+                      title: '性别',
+                      dataIndex: 'sex',
+                      key: 'sex',
+                      render: (v) => (v === 1 ? '男' : '女'),
+                    },
+                    {
+                      title: '生日',
+                      dataIndex: 'birthDay',
+                      key: 'birthDay',
+                      render: (val) => <DateDisplay.DateDisplay10 value={val} />,
+                    },
+                    {
+                      title: '所在部门',
+                      dataIndex: 'deptName',
+                      key: 'deptName',
+                    },
+                    {
+                      title: '身高',
+                      dataIndex: 'height',
+                      key: 'height',
+                    },
+                    {
+                      title: '体重',
+                      dataIndex: 'width',
+                      key: 'width',
+                    },
+                    {
+                      title: '籍贯',
+                      dataIndex: 'hometown',
+                      key: 'hometown',
+                    },
+                    {
+                      title: '地址',
+                      dataIndex: 'address',
+                      key: 'address',
+                    },
+                  ]}
+                  loading={isPending}
+                  dataSource={data}
+                />
+              </Card>
+            );
+          }
+
+          const Test8Dict = {
+            initStatic() {},
+            initRemote() {
+              Dict.handlers.Test8Dict = () => () =>
+                new Promise((resolve) => {
+                  setTimeout(() => {
+                    resolve(
+                      Array.from({ length: 10 }).map((v, index) => ({
+                        id: Mock.mock('@guid'),
+                        name: Mock.mock('@name'),
+                        sex: `${(index + 1) % 2}`,
+                        birthDay: Mock.mock('@now'),
+                        deptName: Mock.mock('@name'),
+                        height: Mock.mock({
+                          'number|+1': 202,
+                        }),
+                        width: Mock.mock({
+                          'number|+1': 202,
+                        }),
+                        hometown: Mock.mock('@city'),
+                        address: Mock.mock('@region'),
+                      })),
+                    );
+                  }, 3000);
+                });
+            },
+          };
+
+          // 初始化
+          Dict.init([Test8Dict], {});
 
           // 使用字典
           return <Test />;
