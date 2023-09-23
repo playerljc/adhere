e0db1f0c2ba241b5e35f6b644bc91896427f72f0
playerljc <playerljclzq@gmail.com>
2023-09-23 20:52:59 +0800

    style(adhere-ui-searchtable): 去掉无用的输出

--- packages/adhere-ui-searchtable/es/Extension/SearchAndPaginParams/routeListen.d.ts
+++ packages/adhere-ui-searchtable/es/Extension/SearchAndPaginParams/routeListen.d.ts
@@ -1,11 +1,5 @@
-/**
- * Listener
- * @param e
- * @param _history
- * @constructor
- */
-declare function Listener(e: any, _history: any): void;
-declare namespace Listener {
-    var getCode: () => (() => void) | undefined;
-}
+declare const Listener: {
+    (e: any, _history: any): void;
+    getCode(): (() => void) | undefined;
+};
 export default Listener;
