c72c53e2d87736ba1bbdbd1e77903c9e1065c51b
playerljc <playerljclzq@gmail.com>
2023-09-23 12:42:02 +0800

    fix(adhere-ui-searchtable): 去掉InputHOC

--- packages/adhere-ui-searchtable/es/Extension/SearchAndPaginParams/routeListen.d.ts
+++ packages/adhere-ui-searchtable/es/Extension/SearchAndPaginParams/routeListen.d.ts
@@ -1,5 +1,11 @@
-declare const Listener: {
-    (e: any, _history: any): void;
-    getCode(): (() => void) | undefined;
-};
+/**
+ * Listener
+ * @param e
+ * @param _history
+ * @constructor
+ */
+declare function Listener(e: any, _history: any): void;
+declare namespace Listener {
+    var getCode: () => (() => void) | undefined;
+}
 export default Listener;
