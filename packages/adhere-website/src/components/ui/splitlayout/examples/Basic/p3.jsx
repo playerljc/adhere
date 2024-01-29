c84ed643d5ae2eb6b7da14216ffcdae629f41065
playerljc <playerljclzq@gmail.com>
2024-01-29 15:52:57 +0800

    demo(adhere-website): 嵌套中加入更多的例子

--- packages/adhere-website/src/components/ui/splitlayout/examples/Basic/p3.jsx
+++ packages/adhere-website/src/components/ui/splitlayout/examples/Basic/p3.jsx
@@ -8,17 +8,25 @@ export default () => (
   <>
     <FlexLayout direction="horizontal" style={{ height: 200, border: '1px solid #ccc' }}>
       <Fixed style={{ width: 30 }} />
+
       <SplitLayout />
+
       <Auto>
         <FlexLayout>
           <Fixed style={{ height: 30 }} />
+
           <SplitLayout style={{ width: 'auto' }} />
+
           <Auto />
+
           <SplitLayout style={{ width: 'auto' }} />
+
           <Fixed style={{ height: 30 }} />
         </FlexLayout>
       </Auto>
+
       <SplitLayout />
+
       <Fixed style={{ width: 30 }} />
     </FlexLayout>
 
@@ -26,18 +34,44 @@ export default () => (
 
     <FlexLayout style={{ height: 200, border: '1px solid #ccc' }}>
       <Fixed style={{ height: 30 }} />
+
       <SplitLayout style={{ width: 'auto' }} />
+
       <Auto>
         <FlexLayout direction="horizontal">
           <Fixed style={{ width: 30 }} />
+
           <SplitLayout />
+
           <Auto />
+
           <SplitLayout />
+
           <Fixed style={{ width: 30 }} />
         </FlexLayout>
       </Auto>
+
       <SplitLayout style={{ width: 'auto' }} />
+
       <Fixed style={{ height: 30 }} />
+    </FlexLayout>
+
+    <Space size={30} />
+
+    <FlexLayout direction="horizontal" style={{ height: 200, border: '1px solid #ccc' }}>
+      <Auto />
+
+      <SplitLayout />
+
+      <Fixed style={{ width: 450 }}>
+        <FlexLayout direction="horizontal" style={{ height: 200 }}>
+          <Auto />
+
+          <SplitLayout />
+
+          <Fixed style={{ width: 50 }} />
+        </FlexLayout>
+      </Fixed>
     </FlexLayout>
   </>
 );
