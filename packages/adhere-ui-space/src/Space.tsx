b2584e5ae0f3a2fb0446c027492fa590cfb8f83c
playerljc <playerljclzq@gmail.com>
2024-06-12 16:15:12 +0800

    fix(adhere-ui-space): 间距值修改

--- packages/adhere-ui-space/src/Space.tsx
+++ packages/adhere-ui-space/src/Space.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React, { memo, useCallback, useContext, useMemo } from 'react';
+import React, { CSSProperties, memo, useContext, useMemo } from 'react';
 
 import ConfigProvider from '@baifendian/adhere-ui-configprovider';
 
@@ -15,31 +15,31 @@ const selectorPrefix = 'adhere-ui-space';
  * @constructor
  */
 const InternalSpace = memo<SpaceProps>((props) => {
-  const { className = '', style = {}, direction = 'horizontal', size = 20 } = props;
+  const { className = '', style = {}, direction = 'horizontal', size = 40 } = props;
 
   const { media } = useContext(ConfigProvider.Context);
 
   const value = useMemo(() => getValue(media, size), [media, size]);
 
-  const getStyle = useCallback(() => {
+  const targetStyle = useMemo<CSSProperties>(() => {
     if (direction === 'horizontal') {
       return {
         display: 'inline-block',
         height: '100%',
-        marginRight: `0 ${value}`,
+        marginRight: `${value}`,
       };
     }
 
     return {
       width: '100%',
-      marginTop: `${value} 0`,
+      marginTop: `${value}`,
     };
   }, [direction, size]);
 
   return (
     <div
       className={classNames(selectorPrefix, className ?? '')}
-      style={{ ...getStyle(), ...(style ?? {}) }}
+      style={{ ...targetStyle, ...(style ?? {}) }}
     />
   );
 });
