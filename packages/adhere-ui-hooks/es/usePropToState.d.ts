727efaef88247a0c57485170bf1e033ecfba3e2f
playerljc <playerljclzq@gmail.com>
2024-08-27 16:32:17 +0800

    fix(adhere-ui-hooks): usePropToState的函数返回值修改

--- packages/adhere-ui-hooks/es/usePropToState.d.ts
+++ packages/adhere-ui-hooks/es/usePropToState.d.ts
@@ -1,7 +1,7 @@
-/// <reference types="react" />
+import { Dispatch, SetStateAction } from 'react';
 /**
  * usePropToState
  * @description 将props中的某值转换成state，用户在组件中对props进行更新使用，用props同步state
  */
-declare function usePropToState<T>(propValue: T): (T | import("react").Dispatch<import("react").SetStateAction<T>>)[];
+declare function usePropToState<T>(propValue: T): [T, Dispatch<SetStateAction<T>>];
 export default usePropToState;
--- packages/adhere-ui-hooks/src/usePropToState.ts
+++ packages/adhere-ui-hooks/src/usePropToState.ts
@@ -1,11 +1,11 @@
 import { useUpdateEffect } from 'ahooks';
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 
 /**
  * usePropToState
  * @description 将props中的某值转换成state，用户在组件中对props进行更新使用，用props同步state
  */
-function usePropToState<T>(propValue: T) {
+function usePropToState<T>(propValue: T): [T, Dispatch<SetStateAction<T>>] {
   const [value, setValue] = useState<T>(propValue);
 
   useUpdateEffect(() => {
