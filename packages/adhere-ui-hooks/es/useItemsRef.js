b7dfb92fb0e43350e35dffc0759397a5abdcc9b6
playerljc <playerljclzq@gmail.com>
2024-09-04 17:46:55 +0800

    feat(adhere-ui-hooks): 重新编译

--- packages/adhere-ui-hooks/es/useItemsRef.js
+++ packages/adhere-ui-hooks/es/useItemsRef.js
@@ -1,2 +1,2 @@
-import{useRef}from"react";function useItemsRef(){var e=useRef(null);function n(){return e.current||(e.current=new Map),e.current}return{get:function(e){var r=n();return null==r?void 0:r.get(e)},set:function(e,r){var t;return null==(t=n())?void 0:t.set(e,r)}}}export default useItemsRef;
+import{useRef}from"react";function useItemsRef(){var e=useRef(null);function n(){return e.current||(e.current=new Map),e.current}return{get:function(e){var t=n();return null==t?void 0:t.get(e)},set:function(e,t){var r;return t?null==(r=n())?void 0:r.set(e,t):(null!=(r=n())&&r.delete(e),n())}}}export default useItemsRef;
 //# sourceMappingURL=useItemsRef.js.map
