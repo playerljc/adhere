24478e6b37d3102050b44767b53452f061003e44
playerljc <playerljclzq@gmail.com>
2024-10-07 15:12:31 +0800

    test(adhere-mobile-ui-anthoc): 加入TreeSelect的e2e

---
+++ packages/adhere-mobile-ui-anthoc/e2e/TreeSelect/TreeShowChildSelect.jsx
@@ -0,0 +1,63 @@
+// import { ScanCodeOutline } from 'antd-mobile-icons';
+import React, { useState } from 'react';
+
+import Util from '@baifendian/adhere-util';
+
+import { TreeSelect } from '../../src';
+
+import '../../src/index.less';
+
+function generateTree(depth, width, currentDepth = 1, parentKey = '0') {
+  // 基本结构
+  const tree = [];
+
+  // 当深度超过指定的深度时，返回空 children
+  if (currentDepth > depth) {
+    return tree;
+  }
+
+  for (let i = 0; i < width; i++) {
+    const key = `${parentKey}-${i}`;
+    tree.push({
+      key: key,
+      title: `Node ${key}`,
+      children: generateTree(depth, width, currentDepth + 1, key), // 递归生成子节点
+    });
+  }
+
+  return tree;
+}
+
+// 生成深度为 3，宽度为 3 的树形数据
+const treeData = generateTree(3, 3);
+
+const flat = Util.treeToArray(
+  treeData,
+  {
+    parentIdAttr: 'pId',
+    rootParentId: 0,
+  },
+  'key',
+);
+
+export default () => {
+  const [value, setValue] = useState([]);
+
+  return (
+    <div style={{ padding: 20, width: '100%', height: '100%', overflowY: 'auto' }}>
+      <TreeSelect.TreeShowChildSelect
+        treeData={treeData}
+        // treeDataSimpleMode
+        size="middle"
+        checkStrictly
+        value={value}
+        onExpand={function () {}}
+        onSelect={function () {}}
+        onChange={function () {
+          debugger;
+        }}
+        // icon={() => <ScanCodeOutline />}
+      />
+    </div>
+  );
+};
