5d1543202868fdf6c8d9edf59af3417f62d754ab
playerljc <playerljclzq@gmail.com>
2025-07-23 12:43:57 +0800

    feat(adhere-e2e): 加入dayjs设置

--- packages/adhere-e2e/src/index.ts
+++ packages/adhere-e2e/src/index.ts
@@ -1,3 +1,8 @@
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import updateLocale from 'dayjs/plugin/updateLocale';
+import utc from 'dayjs/plugin/utc';
+
 import AdapterScreen from '@baifendian/adhere-util-adapterscreen';
 
 import Mobile from './mobile';
@@ -7,6 +12,22 @@ import { isUseMedia } from './util';
 if (isUseMedia()) {
   AdapterScreen.flexible();
 }
+
+/**
+ * dayjsSetting
+ * @description dayjs的设置
+ */
+function dayjsSetting() {
+  // 设置LTS
+  dayjs.extend(updateLocale);
+
+  // 设置dayjs的时区
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+  dayjs.tz.setDefault(Intl.DateTimeFormat().resolvedOptions().timeZone);
+}
+
+dayjsSetting();
 
 export default {
   PC,
