da364fd8fae43a381ddf41c427e052e6276afcc9
playerljc <playerljc@163.com>
2025-09-11 07:01:29 +0800

    fix(adhere-ui-messagedialog): TriggerPrompt.tsx的actions参数修改

--- packages/adhere-ui-messagedialog/src/TriggerPrompt.tsx
+++ packages/adhere-ui-messagedialog/src/TriggerPrompt.tsx
@@ -8,7 +8,7 @@ import type { TriggerPromptHandle, TriggerPromptProps } from './types';
 /**
  * TriggerPrompt组件
  * 带确认按钮的触发器组件
- * 
+ *
  * @param props - 组件属性
  * @param props.onSubmit - 提交回调函数
  * @param props.modalConfig - 模态框配置
@@ -17,21 +17,25 @@ import type { TriggerPromptHandle, TriggerPromptProps } from './types';
  * @returns 触发器提示组件
  */
 const TriggerPrompt = forwardRef<TriggerPromptHandle, TriggerPromptProps>(
-  ({ onSubmit, modalConfig, okText, ...props }, ref) => {
+  ({ onSubmit, modalConfig, okText, actions, ...props }, ref) => {
     return (
       <Trigger
         ref={ref}
         {...props}
         modalConfig={modalConfig}
-        actions={[
-          {
-            key: 'submit',
-            // @ts-ignore
-            type: 'primary',
-            children: okText ?? Intl.get('confirm'),
-            onClick: () => onSubmit?.() ?? Promise.resolve(),
-          },
-        ]}
+        actions={
+          actions
+            ? actions
+            : [
+                {
+                  key: 'submit',
+                  // @ts-ignore
+                  type: 'primary',
+                  children: okText ?? Intl.get('confirm'),
+                  onClick: () => onSubmit?.() ?? Promise.resolve(),
+                },
+              ]
+        }
       />
     );
   },
