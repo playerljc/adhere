ba26b2dd4ae68cfb6cbecbbe4f1c4acc17f1fd38
playerljc <playerljc@163.com>
2025-11-24 14:49:20 +0800

    feat(.eslintrc.js): eslint优化

--- packages/adhere-website/.eslintrc.js
+++ packages/adhere-website/.eslintrc.js
@@ -1,20 +1,14 @@
+const base = require('../../.eslintrc.js');
+
 module.exports = {
-  extends: [require.resolve('@umijs/fabric/dist/eslint')],
-  plugins: ['unused-imports'],
-  globals: {
-    page: true,
-    APP_ENV: true,
-    NO_MOCK: true,
-  },
+  ...base,
   rules: {
-    'import/no-unresolved': 0,
-    'no-unused-expressions': 0,
-    'import/no-extraneous-dependencies': 0,
-    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
-    ],
+    ...base.rules,
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'react-hooks/exhaustive-deps': 'error',
+    'import/order': 'off',
+    'no-debugger': 'error',
+    'no-alert': 'warn',
+    'react/jsx-no-useless-fragment': 'warn',
   },
 };
