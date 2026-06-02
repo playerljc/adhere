981b2bc4bcb3026e2845846ed87f54f4bb14ff0f
playerljc <playerljc@163.com>
2026-06-02 17:58:25 +0800

    feat(adhere-ui-form-design): add FileUpload component and related functionalities
    - Introduced FileUpload component with rendering and action handling capabilities.
    - Added UploadDataSourceManagerFormItem for managing upload data sources.
    - Created UploadEvents and UploadListType dictionaries for event handling and list type selection.
    - Updated ToDoList documentation to reflect the addition of the FileUpload component.
    - Enhanced styling and integration within the existing form design framework.

---
+++ packages/adhere-ui-form-design/src/Dict/UploadEvents.ts
@@ -0,0 +1,13 @@
+// UploadEvents
+
+export const UploadEvents = {
+  handler: () => {
+    return [
+      { label: 'onDrop', value: 'onDrop' },
+      { label: 'onDownload', value: 'onDownload' },
+      { label: 'onPreview', value: 'onPreview' },
+      { label: 'onRemove', value: 'onRemove' },
+    ];
+  },
+};
+
